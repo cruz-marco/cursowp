2db03bbd9bd087b3d8040f5181453afb82cbafae
Marco Cruz <marco_cruz@hotmail.com.br>
2024-03-19 21:46:02 -0300

    mudanças no rodapé, adição de novos ícones, imagens e plugins.

---
+++ wp-content/plugins/filebird/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ninjateam/filebird',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '4c460d49e7ab9da6a0f866b35ba16b3452a58d17',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'enshrined/svg-sanitize' => array(
+            'pretty_version' => '0.15.4',
+            'version' => '0.15.4.0',
+            'reference' => 'e50b83a2f1f296ca61394fe88fbfe3e896a84cf4',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../enshrined/svg-sanitize',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'ninjateam/filebird' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '4c460d49e7ab9da6a0f866b35ba16b3452a58d17',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
